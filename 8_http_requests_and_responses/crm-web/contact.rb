d23738deda3c08a99f4b63527d8b744db00ea1d1
Sandeep Dhanjani <sandeepdhanjani@gmail.com>
2014-04-08 19:04:45 -0400

    finally done

--- 8_http_requests_and_responses/crm-web/contact.rb
+++ 8_http_requests_and_responses/crm-web/contact.rb
@@ -1,20 +1,4 @@
-class Contact
 
-  attr_accessor :id
-  attr_accessor :first_name, :last_name, :email, :note
-
-  def initialize(first_name, last_name, email, note)
-    @first_name = first_name
-    @last_name = last_name
-    @email = email
-    @note = note
-  end
-
-  def to_s
-      "ID: #{@id}\n First Name: #{@first_name}\n Last Name: #{@last_name}\n Email: #{@email}\n Notes:#{@note}"
-  end
-
-end
 
 
 # def add_new_contact
--- 8_http_requests_and_responses/crm-web/crm.rb
+++ 8_http_requests_and_responses/crm-web/crm.rb
@@ -1,15 +1,44 @@
-require_relative 'contact'
-require_relative 'rolodex'
+# require_relative 'rolodex'
 require 'sinatra'
-# require 'data_mapper'
+require 'data_mapper'
 
-# DataMapper.setup(:default, "sqlite3:database.sqlite3")
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
+
+class Contact
+
+  # attr_accessor :id
+  # attr_accessor :first_name, :last_name, :email, :note
+
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :first_name, String
+  property :last_name, String
+  property :email, String
+  property :note, String
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 
-@@rolodex = Rolodex.new
+# def initialize(first_name, last_name, email, note)
+#   @first_name = first_name
+#   @last_name = last_name
+#   @email = email
+#   @note = note
+# end
 
-@@rolodex.add_new_contact(Contact.new("Sandeep", "Dhanjani", "sd@gmail.com", "What a guy!"))
-@@rolodex.add_new_contact(Contact.new("Matt", "Holtom", "matt@gmail.com", "What's up!"))
+# def to_s
+#   "ID: #{@id}\n First Name: #{@first_name}\n Last Name: #{@last_name}\n Email: #{@email}\n Notes:#{@note}"
+# end
+
+# end
+
+# @@rolodex = Rolodex.new
+
+# @@rolodex.add_new_contact(Contact.new("Sandeep", "Dhanjani", "sd@gmail.com", "What a guy!"))
+# @@rolodex.add_new_contact(Contact.new("Matt", "Holtom", "matt@gmail.com", "What's up!"))
 
 get '/' do
   @crm_app_name = "My CRM"
@@ -18,10 +47,11 @@ end
 
 get "/contacts" do
   if params.empty?
-    @contacts = @@rolodex.contacts
+    @contacts = Contact.all
   else
-    @contacts = @@rolodex.find(params[:trait], params[:search_goal])
-   end
+    @contacts = Contact.all(params[:trait] => params[:search_goal])
+  end
+
   erb :contacts
 end
 
@@ -34,36 +64,78 @@ get '/contacts/search' do
 end
 
 get '/contacts/:id/edit' do
-  @contact = @@rolodex.find("id", params[:id].to_i)[0]
+  @contact = Contact.get(params[:id].to_i)
   erb :edit_contact
 end
 
-post '/contacts/:id' do
-  @@rolodex.edit_contact(params[:id], params[:first_name], params[:last_name], params[:email], params[:note])
-  @contacts = @@rolodex.contacts
-  erb :contacts
+get '/contacts/:id' do
+  @contact = Contact.get(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
+  # Contact.(params[:id], params[:first_name], params[:last_name], params[:email], params[:note])
+  # @contacts = Contact.get
+  # erb :contacts
+# end
+
 post '/contacts' do
-  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  @@rolodex.add_new_contact(new_contact)
+  @contact = Contact.create(
+    :first_name => params[:first_name],
+    :last_name => params[:last_name],
+    :email => params[:email],
+    :note => params[:note]
+    )
+
+  # new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  # @@rolodex.add_new_contact(new_contact)
   redirect to('/contacts')
+
 end
 
 put '/contacts/:id' do
-  @@rolodex.edit_contact(params[:id], params[:first_name], params[:last_name], params[:email], params[:note])
+  @contact = Contact.get(params[:id].to_i)
+  @contact.first_name = params[:first_name]
+  @contact.last_name = params[:last_name]
+  @contact.email = params[:email]
+  @contact.note = params[:note]
+  @contact.save
+
   redirect to("/contacts/#{params[:id]}")
 end
 
 get '/contacts/:id' do
-  @contact = @@rolodex.find("id", params[:id].to_i)[0]
+  @contact = Contact.find("id", params[:id].to_i)[0]
   erb :show_contact
 end
 
 delete '/contacts/:id' do
-  @contact = @@rolodex.remove_contact(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
+  @contact.destroy
   redirect to ("/contacts")
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
